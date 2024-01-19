3193fd44fa2164e998dcaf60b1690f32f88d54d6
nsleite <nivaldosleite@hotmail.com>
2024-01-19 20:36:11 -0300

    fully functional

--- bank.py
+++ bank.py
@@ -9,45 +9,68 @@ menu = """
 =>"""
 
 saldo = 0
-extrato = ''
+extrato = []
 numero_saques = 0
 LIMITE_SAQUES = 3
 SAQUE_MAXIMO = 500
 operacao = []
 
-def depositar(saldo, operacao):
-    global deposito
+def depositar(saldo, extrato):
     try:
         deposito = float(input("insira o valor a ser depositado: "))
     except: 
-        print("valor inalido! ")
-        return
+        print("valor inválido!")
+        return saldo
+    if deposito < 0:
+        print("valor inválido!")
+        return saldo
     saldo += deposito
-    operacao.append(['Depósito', deposito, saldo])
-    print(f'Deposito: R${deposito}, saldo atual: R${saldo}')
-    return saldo, operacao
+    extrato.append(['Depósito', deposito, saldo])
+    print(f'Deposito: R${deposito:.2f}, saldo atual: R${saldo:.2f}')
+    return saldo
 
-def sacar():
-    pass
-def mostra_extrato(operacao):
-    for op, val, sal in operacao:
-        print(f'operação: {op}, Valor: R${val}, saldo: {sal}')
+def sacar(saldo, extrato):
+    global numero_saques
+    try:
+        saque = float(input("insira o valor a ser sacado: "))
+    except: 
+        print("valor inválido!")
+        return saldo
+    if saque < 0:
+        print("valor inválido!")
+        return saldo
+    if saque > saldo:
+        print("saque excede o saldo em conta")
+        return saldo
+    if saque > SAQUE_MAXIMO:
+        print("valor excede o saque máximo de R$ 500.00")
+        return saldo
+    if numero_saques >= 3:
+        print("limite de saques 3 diários excedido")
+        return saldo
+    saldo -= saque
+    numero_saques += 1
+    extrato.append(['Saque', saque, saldo])
+    print(f'Saque: R${saque:.2f}, saldo atual: R${saldo:.2f}')
+    return saldo
+    
+def mostra_extrato(extrato):
+    if len(extrato) == 0:
+        print("Não foram realizadas movimentações.")
+        return
+    for op, val, sal in extrato:
+        print(f'operação: {op}, Valor: R${val:.2f}, saldo: R${sal:.2f}')
     return
 
 
 while True:
-
     opcao = input(menu)
-
     if opcao == "d":
-        saldo, operacao = depositar(saldo, operacao)
-
+        saldo = depositar(saldo, extrato)
     elif opcao == "s":
-        sacar()
-
+        saldo = sacar(saldo, extrato)
     elif opcao == "e":
-        mostra_extrato(operacao)
-
+        mostra_extrato(extrato)
     elif opcao == "q":
         break
 
